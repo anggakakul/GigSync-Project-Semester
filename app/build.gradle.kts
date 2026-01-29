5b65ba50b0c7099e6c1d6f46bc496a96fce549a6
Angga Yudistira <159276733+anggakakul@users.noreply.github.com>
2026-01-29 14:18:28 +0800

    Versi Final Inpo Gigs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-parcelize")
+    id("kotlin-kapt")
 }
 
 android {
@@ -35,6 +37,9 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -46,4 +51,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
 }
--- app/src/main/java/com/anggayoedis/gigsync/MainActivity.kt
+++ app/src/main/java/com/anggayoedis/gigsync/MainActivity.kt
@@ -1,20 +1,43 @@
 package com.anggayoedis.gigsync
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.anggayoedis.gigsync.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container_main, HomeFragment())
+                .commit()
         }
+
+        // --- FITUR BARU: KONFIRMASI KELUAR ---
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                // Cek apakah user sedang membuka halaman Detail? (Tumpukan Fragment > 0)
+                if (supportFragmentManager.backStackEntryCount > 0) {
+                    // Kalau ya, biarkan dia kembali ke halaman sebelumnya
+                    supportFragmentManager.popBackStack()
+                } else {
+                    // Kalau user sudah di Home (Halaman Depan), tanya dulu sebelum keluar
+                    showExitDialog()
+                }
+            }
+        })
+    }
+
+    private fun showExitDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Keluar Aplikasi? 🚪")
+            .setMessage("Apakah kamu yakin ingin menutup Inpo Gigs?")
+            .setPositiveButton("Ya, Keluar") { _, _ -> finish() } // Tutup Aplikasi
+            .setNegativeButton("Batal", null) // Gak jadi keluar
+            .show()
     }
 }
